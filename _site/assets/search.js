aa6df7b8530ad313b769cb9c28d7bfe94d459a07
Raayan Pillai <raayanp01@gmail.com>
2019-04-29 19:04:29 -0400

    Setting up general site

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':'Post+with+Header+Image','url':'%2F2018%2F06%2F01%2Fheader-image.html'},{'title':'Welcome','url':'%2F2018%2F07%2F01%2Fwelcome.html'},{'title':'Night+King+%3D+Complex+Villain','url':'%2F2019%2F04%2F29%2Fgots8e3.html'}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':'Test','url':'%2F2019%2F04%2F29%2Fcode-test.html'},{'title':'Night+King+%3D+Complex+Villain','url':'%2F2019%2F04%2F29%2Fgots8e3.html'}]};
